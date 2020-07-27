9bc9de1e23aa8323d99c20eb698a32998a72deb0
sudeepthinittala <17pa1a05b5@vishnu.edu.in>
2020-07-27 21:38:12 +0530

    Updates

---
+++ precache-manifest.1473b9cc711dc3e9127d8595294440a7.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fd170e17ea4a89e5792e09fc55866c2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3353e42aaed9aa147728",
+    "url": "/static/css/2.3657568d.chunk.css"
+  },
+  {
+    "revision": "6d6fef8334fb7588bd54",
+    "url": "/static/css/main.b4b68261.chunk.css"
+  },
+  {
+    "revision": "3353e42aaed9aa147728",
+    "url": "/static/js/2.f4518288.chunk.js"
+  },
+  {
+    "revision": "7979eeaf519f92e9fad084e05c681841",
+    "url": "/static/js/2.f4518288.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d6fef8334fb7588bd54",
+    "url": "/static/js/main.e8fb16e6.chunk.js"
+  },
+  {
+    "revision": "df7d820ded1d4db0ff91",
+    "url": "/static/js/runtime-main.410ddc6b.js"
+  },
+  {
+    "revision": "77265175e11b6d5c36eeb9db52d7508d",
+    "url": "/static/media/Screenshot (162).77265175.png"
+  },
+  {
+    "revision": "141d3db5e9cff02f3552906384c673e2",
+    "url": "/static/media/Screenshot (163).141d3db5.png"
+  },
+  {
+    "revision": "82f90a6f952ab32684d3c1dcb5267b70",
+    "url": "/static/media/Screenshot (164).82f90a6f.png"
+  },
+  {
+    "revision": "0701dc65b59a62030c9d16c21129f08a",
+    "url": "/static/media/Screenshot (165).0701dc65.png"
+  },
+  {
+    "revision": "080a62a8e0f0a7e1c04f58418fe87a35",
+    "url": "/static/media/Screenshot (166).080a62a8.png"
+  },
+  {
+    "revision": "7f4ca6711674233dad57d0f73a0cce3e",
+    "url": "/static/media/bg4.7f4ca671.jpg"
+  },
+  {
+    "revision": "6a2ddad1092a0a1c326b6d0e738e682b",
+    "url": "/static/media/materialdesignicons-webfont.6a2ddad1.ttf"
+  },
+  {
+    "revision": "8a67040660444e8d8ee82a406d580d8f",
+    "url": "/static/media/materialdesignicons-webfont.8a670406.eot"
+  },
+  {
+    "revision": "c32505e8c654310ece1da4e1c10eeb57",
+    "url": "/static/media/materialdesignicons-webfont.c32505e8.woff"
+  },
+  {
+    "revision": "dc85ceeb0daba687e36d8dde4ed4d352",
+    "url": "/static/media/materialdesignicons-webfont.dc85ceeb.woff2"
+  }
+]);
